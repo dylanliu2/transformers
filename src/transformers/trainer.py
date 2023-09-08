2ca17012b368a6202a1c5923442269b3ee8b5ef4
dylanliu2 <106902132+dylanliu2@users.noreply.github.com>
2023-09-08 17:30:19 +0800

    Update trainer.py
    better trainer

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -969,21 +969,30 @@ class Trainer:
         if self.optimizer is None:
             decay_parameters = get_parameter_names(opt_model, ALL_LAYERNORM_LAYERS)
             decay_parameters = [name for name in decay_parameters if "bias" not in name]
-            optimizer_grouped_parameters = [
-                {
-                    "params": [
-                        p for n, p in opt_model.named_parameters() if (n in decay_parameters and p.requires_grad)
-                    ],
-                    "weight_decay": self.args.weight_decay,
-                },
-                {
-                    "params": [
-                        p for n, p in opt_model.named_parameters() if (n not in decay_parameters and p.requires_grad)
-                    ],
-                    "weight_decay": 0.0,
-                },
-            ]
 
+            optimizer_grouped_parameters = []
+            for n, p in opt_model.named_parameters():
+                if (n in decay_parameters and p.requires_grad):
+                    optimizer_grouped_parameters.append({
+                        "params": [
+                            p for n, p in opt_model.named_parameters() if (n in decay_parameters and p.requires_grad)
+                        ],
+                        "weight_decay": self.args.weight_decay,
+                    })
+                if (n not in decay_parameters and p.requires_grad):
+                        optimizer_grouped_parameters.append({
+                            "params": [
+                                p for n, p in opt_model.named_parameters() if (n in decay_parameters and p.requires_grad)
+                            ],
+                            "weight_decay": self.args.weight_decay,
+                        })
+
+            if self.args.lr_decay != 1:
+                lr = self.arg.learning_rate
+                for param in optimizer_grouped_parameters[::-1]:
+                    param["lr"] = lr
+                    lr *= self.args.lr_decay
+                
             optimizer_cls, optimizer_kwargs = Trainer.get_optimizer_cls_and_kwargs(self.args)
 
             if self.sharded_ddp == ShardedDDPOption.SIMPLE:
